04db8808f1642cb4878dbbeff48b2f1400ae8c02
ElyTilley <etilley1@vcd-ch204-18.eastern.ewu.edu>
2017-02-22 13:45:03 -0800

    Randomized Homepage Image
    Got Java to randomize images

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 // YOUR SCRIPTS GO HERE
+
+// Store 3 Images
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// Get Random Number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+// Display Random Images
+$('img').attr('src', eval('img'+ numRand));
